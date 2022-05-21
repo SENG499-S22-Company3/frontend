e41645a6c5d78e94a911b781143d61e2406c2dac
Jayden Chan <jaydencn7@gmail.com>
2022-05-20 20:25:27 -0700

    JC: Dark mode by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  config,
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
