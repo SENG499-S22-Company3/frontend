e506a92d27f7f5a353fdcd416ba4f32e487d7502
Jayden Chan <jaydencn7@gmail.com>
2022-05-30 17:02:11 -0700

    JC: More work on nav header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,41 +1,51 @@
 import { Flex, Text, Link } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import React from "react";
-import { useLoginStore } from "../stores/login";
+import { useLoginStore, LoginStore } from "../stores/login";
 
-const LoginStatus = () => {
-  const loginState = useLoginStore();
+const LoginStatus = (props: { loginState: LoginStore }) => {
+  const { loginState } = props;
   if (loginState.loggedIn && loginState.user !== undefined) {
     return <Flex>Hello {loginState.user.name}</Flex>;
   }
-  return (
-    <Link as={ReactRouterLink} to="/login">
-      Login
-    </Link>
-  );
+  return <NavLink to="/login" desc="Login" />;
 };
 
+const NavLink = (props: { to: string; desc: string }) => (
+  <Link as={ReactRouterLink} to={props.to} mr={4}>
+    <b>{props.desc}</b>
+  </Link>
+);
+
 export const NavHeader = (props: {}) => {
+  const loginState = useLoginStore();
   return (
     <Flex
       w="100vw"
       h="50px"
-      bg="#111"
+      bg="#232b3b"
       alignItems="center"
       justifyContent="space-between"
       px={3}
     >
       <Flex>
-        <Text mr={3}>
-          <b>Schedulater</b>
+        {/* Eventually this will be replaced with a logo or something nicer */}
+        <Text mr={4}>
+          <b>SchedulaterLogoHere</b>
         </Text>
-        <Link mr={2}>Home</Link>
-        <Link mr={2}>Admin</Link>
-        <Link mr={2}>Schedules</Link>
-        <Link mr={2}>Calendar</Link>
-        <Link mr={2}>Survey Results</Link>
+        <NavLink to="/" desc="Home" />
+        {loginState.user && loginState.user.roles.includes("admin") && (
+          <>
+            <NavLink to="/dashboard" desc="Admin Dashboard" />
+            <NavLink to="/generate" desc="Generate Schedules" />
+          </>
+        )}
+        <NavLink to="/schedule" desc="View Schedules" />
+        <NavLink to="/profileManagement" desc="Profile Management" />
+        <NavLink to="/survey" desc="Preferences Survey" />
+        <NavLink to="/surveyresults" desc="Survey Results" />
       </Flex>
-      <LoginStatus />
+      <LoginStatus loginState={loginState} />
     </Flex>
   );
 };
