90035f0282bd01df221f700e95c8986eeeea9c2f
Connor Buchko <connorbuchko2010@yahoo.ca>
2022-06-04 15:52:32 -0700

    incorporating table view with calendar view

---
+++ src/components/ScheduleView.tsx
@@ -0,0 +1,45 @@
+import { Scheduler, View } from "devextreme-react/scheduler";
+import "devextreme/dist/css/dx.dark.css";
+
+var appointment = [
+    {
+      title: "SENG 265",
+      startDate: new Date("2022-06-03T16:00:00.000Z"),
+      endDate: new Date("2022-06-03T16:50:00.000Z"),
+    },
+    {
+      text: "CSC 320",
+      startDate: new Date("2022-06-03T20:00:00.000Z"),
+      endDate: new Date("2022-06-03T21:20:00.000Z"),
+    },
+    {
+      text: "CSC 360",
+      startDate: new Date("2022-05-30T16:00:00.000Z"),
+      endDate: new Date("2022-05-30T17:20:00.000Z"),
+    },
+    {
+      text: "ECE 260",
+      startDate: new Date("2022-05-31T21:00:00.000Z"),
+      endDate: new Date("2022-05-31T21:50:00.000Z"),
+    },
+    {
+      text: "SENG 350",
+      startDate: new Date("2022-05-30T17:30:00.000Z"),
+      endDate: new Date("2022-05-30T18:50:00.000Z"),
+    },
+  ];
+
+  
+export const ScheduleView = () => {
+    return (
+        //the library's TypeScript configuration is broken, not allowing Scheduler to have children
+        //@ts-ignore
+        <Scheduler
+          timeZone="America/Los_Angeles"
+          dataSource={appointment}
+          views={["day", "week"]}
+          defaultCurrentView="week"
+          startDayHour={8}
+        ></Scheduler>
+      );
+};
