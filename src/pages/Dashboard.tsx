b0964ff0de42637049e9cec14b9f553207cc15d3
nicole <nicolelynneudy@gmail.com>
2022-06-07 23:48:02 -0700

    switched dashboard from materialui to chakra

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,106 +1,42 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent'; 
-import Typography from '@material-ui/core/Typography'; 
-import CardActions from '@material-ui/core/CardActions'; 
-import Button from '@material-ui/core/Button'; 
-import Grid from '@material-ui/core/Grid'; 
+import { Flex, HStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { Card } from "../components/Card";
 
 export const Dashboard = () => {
 	const navigate = useNavigate();
 	
   	return (
-	    <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-	    	<Grid item xs={2}>
-			    <Card style={{ height: '25vh' }}>
-			      	<CardContent>
-			        	<Typography variant="h4">
-			          		Survey Results
-			        	</Typography>
-			        	<Typography>
-			          		View the results of the surveys that professors have submitted
-			        	</Typography>
-			      	</CardContent>
-			      	<CardActions>
-			        	<Button 
-			        		style={{color: "#3366BB"}}
-				        		onClick={() => {
-	    							navigate("/surveyresults");
-	  							}}
-  							>
-  								Go Here
-  						</Button>
-			      	</CardActions>
-			    </Card>
-		    </Grid>
-		    <Grid item xs={2}>
-			    <Card style={{ height: '25vh' }}>
-			      	<CardContent>
-			        	<Typography variant="h4">
-			          		Schedule
-			        	</Typography>
-			        	<Typography>
-			          		View class schedules
-			        	</Typography>
-			      	</CardContent>
-			      	<CardActions>
-			        	<Button 
-			        		style={{color: "#3366BB"}}
-				        		onClick={() => {
-	    							navigate("/schedule");
-	  							}}
-  							>
-  								Go Here
-  						</Button>
-			      	</CardActions>
-			    </Card>
-		    </Grid>
-		    <Grid item xs={2}>
-			    <Card style={{ height: '25vh' }}>
-			      	<CardContent>
-			        	<Typography variant="h4">
-			          		Generate
-			        	</Typography>
-			        	<Typography>
-			          		Generate a schedule
-			        	</Typography>
-			      	</CardContent>
-			      	<CardActions>
-			        	<Button 
-			        		style={{color: "#3366BB"}}
-				        		onClick={() => {
-	    							navigate("/generate");
-	  							}}
-  							>
-  								Go Here
-  						</Button>
-			      	</CardActions>
-			    </Card>
-		    </Grid>
-		    <Grid item xs={2}>
-			    <Card style={{ height: '25vh' }}>
-			      	<CardContent>
-			        	<Typography variant="h4">
-			          		Profile Management
-			        	</Typography>
-			        	<Typography>
-			          		Manage profiles of different professors
-			        	</Typography>
-			      	</CardContent>
-			      	<CardActions>
-			        	<Button 
-			        		style={{color: "#3366BB"}}
-				        		onClick={() => {
-	    							navigate("/profileManagement");
-	  							}}
-  							>
-  								Go Here
-  						</Button>
-			      	</CardActions>
-			    </Card>
-		    </Grid>
-	    </Grid>
+	  	<Flex
+	    	w="100%"
+	      	minH="calc(100vh - 5.5rem)"
+	      	pt={30}
+	      	alignItems="center"
+	      	justifyContent="center"
+	      	flexDirection="column"
+	      	mb={32}
+	    >
+      	<Flex mb={32}>
+      		<HStack spacing="24px" alignItems="center" justifyContent="center">
+		  		<Card
+		  			description="View the results of the surveys that professors have submitted"
+		  			navigateTo="/surveyresults"
+				/>
+				<Card
+		  			description="View class schedules"
+		  			navigateTo="/schedule"
+				/>
+				<Card
+		  			description="Generate a schedule"
+		  			navigateTo="/generate"
+				/>
+				<Card
+		  			description="Manage profiles of different professors"
+		  			navigateTo="/profileManagement"
+				/>
+			</HStack>
+		</Flex>
+    </Flex>
   	);
 };
 
