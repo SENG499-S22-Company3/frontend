27e051810e84e7349c9de21a42ba2de4724a5d52
Connor Buchko <connorbuchko2010@yahoo.ca>
2022-06-09 13:25:34 -0700

    prettier

--- src/stores/preferences.ts
+++ src/stores/preferences.ts
@@ -1,19 +1,18 @@
 import { Course } from "./schedule";
 
 export type Professor = {
-    preferences: Preference[];
-    coursesCanTeach: Course[];
-    displayName: string;
-    teachingStatus: string;
-    requiredEquipment: string;
-    hasPEng: boolean;
-    fallTermCourses: number;
-    springTermCourses: number;
-    summerTermCourses: number;
-  };
-  
-  export type Preference = {
-    course: Course;
-    preferenceNum: number;
-  };
-  
+  preferences: Preference[];
+  coursesCanTeach: Course[];
+  displayName: string;
+  teachingStatus: string;
+  requiredEquipment: string;
+  hasPEng: boolean;
+  fallTermCourses: number;
+  springTermCourses: number;
+  summerTermCourses: number;
+};
+
+export type Preference = {
+  course: Course;
+  preferenceNum: number;
+};
