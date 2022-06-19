9487680034bbd405d6d8be2e2aa19ef79328f0c0
Connor Buchko <connorbuchko2010@yahoo.ca>
2022-06-19 16:36:01 -0700

    add util

---
+++ src/utils/weekdayToInt.ts
@@ -0,0 +1,24 @@
+import { Day } from "../stores/schedule";
+
+export const weekdayToInt = (dayOfWeek: Day) => {
+  console.log(dayOfWeek);
+  if (dayOfWeek === Day.MONDAY) {
+    return 0;
+  }
+  if (dayOfWeek === Day.TUESDAY) {
+    return 1;
+  }
+  if (dayOfWeek === Day.WEDNESDAY) {
+    return 2;
+  }
+  if (dayOfWeek === Day.THURSDAY) {
+    return 3;
+  }
+  if (dayOfWeek === Day.FRIDAY) {
+    return 4;
+  }
+  if (dayOfWeek === Day.SATURDAY) {
+    return 5;
+  }
+  return 6;
+};
