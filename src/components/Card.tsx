eb7e9ef002b503396db7ee8d5c598ddc68509f0e
Jayden Chan <jaydencn7@gmail.com>
2022-06-20 14:55:48 -0700

    chore: prettier everything

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-export const Card = (props: { description: string; navigateTo: string; title: string }) => {
+export const Card = (props: {
+  description: string;
+  navigateTo: string;
+  title: string;
+}) => {
   const navigate = useNavigate();
-	
+
   return (
-  	<Box bg='white' h='100%' w='15%' p={8} color='black'>
-  	  <h1 style={ {fontWeight: 'bold'} } > {props.title} </h1>
-  	  {props.description}
-	  <Button 	
-        color='blue.600'
+    <Box bg="white" h="100%" w="15%" p={8} color="black">
+      <h1 style={{ fontWeight: "bold" }}> {props.title} </h1>
+      {props.description}
+      <Button
+        color="blue.600"
         onClick={() => {
-		  navigate(props.navigateTo);
-		}}
-	  >
-		Go Here
-	  </Button>
-	</Box>
+          navigate(props.navigateTo);
+        }}
+      >
+        Go Here
+      </Button>
+    </Box>
   );
 };
-
--- src/utils/calculateCourseRating.ts
+++ src/utils/calculateCourseRating.ts
@@ -1,17 +1,17 @@
 export const calculateCourseRating = (able: string, willing: string) => {
-    if (able === "With Effort" && willing === "Unwilling") {
-      return 20;
-    } else if (able === "Able" && willing === "Unwilling") {
-      return 39;
-    } else if (able === "With Effort" && willing === "Willing") {
-      return 40;
-    } else if (able === "Able" && willing === "Willing") {
-      return 78;
-    } else if (able === "With Effort" && willing === "Very Willing") {
-      return 100;
-    } else if (able === "Able" && willing === "Very Willing") {
-      return 195;
-    } else {
-      return 0;
-    }
-  };
+  if (able === "With Effort" && willing === "Unwilling") {
+    return 20;
+  } else if (able === "Able" && willing === "Unwilling") {
+    return 39;
+  } else if (able === "With Effort" && willing === "Willing") {
+    return 40;
+  } else if (able === "Able" && willing === "Willing") {
+    return 78;
+  } else if (able === "With Effort" && willing === "Very Willing") {
+    return 100;
+  } else if (able === "Able" && willing === "Very Willing") {
+    return 195;
+  } else {
+    return 0;
+  }
+};
