9ede273dfbc4a3d2a8b8b906da3cc11b9ef9e1a8
nicole <nicolelynneudy@gmail.com>
2022-06-23 22:30:53 -0700

    login and logout functionality with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 import { colors } from "./theme/colors";
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
@@ -13,6 +14,7 @@ import { NotFound } from "./pages/NotFound";
 import { Survey } from "./pages/Survey";
 import { NavHeader } from "./components/Nav";
 import { Footer } from "./components/Footer";
+
 const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: false,
@@ -23,20 +25,40 @@ const theme = extendTheme({
   config,
 });
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <NavHeader />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/generate" element={<Generate />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profileManagement" element={<ProfileManagement />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/survey" element={<Survey />} />
-      <Route path="/surveyresults" element={<SurveyResults />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Footer />
-  </ChakraProvider>
-);
+export const App = () => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user") || '{ "user": "" }')
+  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user") || '{ "user": "" }'));
+  }, [navigate]);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <NavHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {user && user["roles"] && user["roles"].includes("admin") && (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/generate" element={<Generate />} />
+            <Route path="/profileManagement" element={<ProfileManagement />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/surveyresults" element={<SurveyResults />} />
+            <Route path="*" element={<NotFound />} />
+          </>
+        )}
+        {user && user["roles"] && user["roles"].includes("user") && (
+          <>
+            <Route path="/survey" element={<Survey />} />
+            <Route path="*" element={<NotFound />} />
+          </>
+        )}
+      </Routes>
+      <Footer />
+    </ChakraProvider>
+  );
+};
