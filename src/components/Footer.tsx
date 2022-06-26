060ff3684e97541befcdba03853bc86d6ef6dd5d
Jayden Chan <jaydencn7@gmail.com>
2022-06-25 17:06:22 -0700

    feat: don't show footer text on small screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
-import { Flex, Text, Image, Box, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 export const Footer = () => {
   const bg = useColorModeValue("gray.100", "gray.700");
+  const isSmall = useBreakpointValue({ base: true, xl: false });
 
   return (
     <Flex
@@ -23,10 +31,12 @@ export const Footer = () => {
           minWidth="40px"
         ></Image>
       </Box>
-      <Text color="gray">
-        Made by SENG 499 students in Company 3 at the University of Victoria
-        during summer 2022.
-      </Text>
+      {!isSmall && (
+        <Text color="gray">
+          Made by SENG 499 students in Company 3 at the University of Victoria
+          during summer 2022.
+        </Text>
+      )}
       <a href="https://github.com/SENG499-S22-Company3">
         <b title="View Source Code on GitHub">View code on GitHub</b>
       </a>
