95d268b1151af94d526fdd816543079b433e1d65
Connor Buchko <connorbuchko2010@yahoo.ca>
2022-06-27 13:53:28 -0700

    fixed schedule times

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -7,3 +7,16 @@ export const formatDate = (date: Date) => {
     month: "long",
   })} ${date.getFullYear()}`;
 };
+
+export const formatTimeString = (date: Date) => {
+  let hours = date.getHours().toString();
+  let minutes = date.getMinutes().toString();
+
+  if (hours.length === 1) {
+    hours = "0" + hours;
+  }
+  if (minutes.length === 1) {
+    minutes = "0" + minutes;
+  }
+  return [hours, minutes];
+};
