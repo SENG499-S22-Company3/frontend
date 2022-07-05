fb499e160ee5a3bb673852c763f377e169db89c2
Adam Kwan <adam876k@gmail.com>
2022-07-04 19:08:54 -0700

    First concept for section choosing

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -4,14 +4,32 @@ import {
   Container,
   Flex,
   FormLabel,
+  Grid,
+  GridItem,
   Heading,
-  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
   Select,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
   useColorModeValue,
   useToast,
 } from "@chakra-ui/react";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import {
+  GroupBase,
+  MultiValue,
+  OptionBase,
+  Select as SelectPlus,
+} from "chakra-react-select";
 
 const GENERATE = gql`
   mutation generate($input: GenerateScheduleInput!) {
@@ -22,11 +40,58 @@ const GENERATE = gql`
   }
 `;
 
+const GET_COURSES = gql`
+  query get_courses {
+    courses(term: SUMMER) {
+      CourseID {
+        subject
+        code
+      }
+    }
+  }
+`;
+
+const temp_course_list = [
+  {
+    label: "SENG 426",
+    value: "SENG 426",
+    colorScheme: "green",
+  },
+  {
+    label: "SENG 440",
+    value: "SENG 440",
+  },
+  {
+    label: "SENG 499",
+    value: "SENG 499",
+  },
+];
+
+interface CoursePreference {
+  course: string;
+  sections: number;
+}
+
+interface CourseOption extends OptionBase {
+  label: string;
+  value: string;
+}
+
 export const Generate = () => {
-  const [generate, { data, loading, error }] = useMutation(GENERATE);
-  const navigate = useNavigate();
   const [year, setYear] = useState("");
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("SPRING");
+  const [selectedCourses, setSelectedCourses] = useState<
+    MultiValue<CourseOption>
+  >([]);
+  const [generate, { data, loading, error }] = useMutation(GENERATE);
+  const {
+    data: courseData,
+    loading: courseLoading,
+    error: courseError,
+  } = useQuery(GET_COURSES, {
+    variables: { term },
+  });
+  const navigate = useNavigate();
 
   const bg = useColorModeValue("gray.50", "gray.700");
 
@@ -39,6 +104,16 @@ export const Generate = () => {
     };
     generate({ variables: { input } });
   };
+
+  const handleCourseChange = (courses: MultiValue<CourseOption>) => {
+    console.log(courses);
+    setSelectedCourses(courses);
+  };
+
+  useEffect(() => {
+    // console.log(courseData);
+    // console.log(courseError);
+  }, [courseData, courseError, courseLoading]);
 
   useEffect(() => {
     if (!loading) {
@@ -73,46 +148,104 @@ export const Generate = () => {
       pt={30}
       alignItems="center"
       justifyContent="center"
-      flexDirection="column"
     >
       <Container
+        minW="900px"
         bg={bg}
         borderRadius={20}
         p={10}
         style={{ boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.40)" }}
         textAlign="center"
+        centerContent
       >
         <Heading mb={4}>Generate Schedule</Heading>
+
         <form onSubmit={onSubmit}>
-          <FormLabel htmlFor="year">Year</FormLabel>
-          <Select
-            placeholder="Select Year"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-            mb={5}
-            w="50%"
-          >
-            <option value="2022">2022</option>
-            <option value="2023">2023</option>
-            <option value="2024">2024</option>
-          </Select>
-          <FormLabel htmlFor="term">Term</FormLabel>
-          <Select
-            placeholder="Select Term"
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-            mb={5}
-            w="50%"
-          >
-            <option value="Spring">Spring</option>
-            <option value="Summer">Summer</option>
-            <option value="Fall">Fall</option>
-          </Select>
-          <FormLabel htmlFor="courses">Courses</FormLabel>
-          <Input
-            id="courses"
-            placeholder="Placeholder for when adding courses ready"
-          />
+          <Grid templateColumns="repeat(2, 1fr)" gap={10}>
+            <GridItem
+              bg="gray.800"
+              p={10}
+              borderRadius={10}
+              flexDir="column"
+              style={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.40)" }}
+            >
+              <Heading mb={4} size="md">
+                Options
+              </Heading>
+
+              <FormLabel htmlFor="year">Year</FormLabel>
+              <Select
+                placeholder="Select Year"
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+                mb={5}
+                w="50%"
+              >
+                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+              </Select>
+              <FormLabel htmlFor="term">Term</FormLabel>
+              <Select
+                placeholder="Select Term"
+                value={term}
+                onChange={(e) => setTerm(e.target.value)}
+                mb={5}
+                w="50%"
+              >
+                <option value="Spring">Spring</option>
+                <option value="Summer">Summer</option>
+                <option value="Fall">Fall</option>
+              </Select>
+              <FormLabel htmlFor="courses">Courses</FormLabel>
+              <SelectPlus<CourseOption, true, GroupBase<CourseOption>>
+                isMulti
+                name="courses"
+                options={temp_course_list}
+                placeholder="Select courses"
+                onChange={handleCourseChange}
+              />
+            </GridItem>
+            <GridItem
+              bg="gray.800"
+              p={10}
+              borderRadius={10}
+              flexDir="column"
+              style={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.40)" }}
+            >
+              <Heading mb={4} size="md">
+                Course Sections
+              </Heading>
+              <Table variant="striped" size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Course</Th>
+                    <Th>Sections</Th>
+                    <Th>Delete</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {selectedCourses.map((course) => (
+                    <Tr key={course.value}>
+                      <Td>{course.value}</Td>
+                      <Td>
+                        <NumberInput defaultValue={1} max={10} min={1} w={20}>
+                          <NumberInputField />
+                          <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                          </NumberInputStepper>
+                        </NumberInput>
+                      </Td>
+                      <Td>
+                        <Button bg="red.500">Delete</Button>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </GridItem>
+          </Grid>
           <Button
             mt={5}
             w="300px"
