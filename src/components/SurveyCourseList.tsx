0b8cbbf7d262865a7c4f839c53962ef65b9a2e04
Adam Kwan <adam876k@gmail.com>
2022-07-08 15:24:58 -0700

    Add filter for demo

--- src/components/SurveyCourseList.tsx
+++ src/components/SurveyCourseList.tsx
@@ -130,37 +130,42 @@ export const SurveyCourseList: React.FC<ChildProps> = (props) => {
           </Thead>
           <Tbody>
             {data.survey.courses.map(
-              (course: PreferenceInterface, index: number) => (
-                <Tr key={"preference-" + index}>
-                  <Td>
-                    {course.subject} {course.code}
-                  </Td>
-                  <Td>{course.term}</Td>
-                  <Td>
-                    <RadioGroup
-                      id="canTeach"
-                      onChange={(v) => handleChange(course, "Able", v)}
-                    >
-                      <Stack direction="row">
-                        <Radio value="Able">Able</Radio>
-                        <Radio value="With Effort">With Effort</Radio>
-                      </Stack>
-                    </RadioGroup>
-                  </Td>
-                  <Td>
-                    <RadioGroup
-                      id="willingTeach"
-                      onChange={(v) => handleChange(course, "Willingness", v)}
-                    >
-                      <Stack direction="row">
-                        <Radio value="Unwilling">Unwilling</Radio>
-                        <Radio value="Willing">Willing</Radio>
-                        <Radio value="Very Willing">Very Willing</Radio>
-                      </Stack>
-                    </RadioGroup>
-                  </Td>
-                </Tr>
-              )
+              (course: PreferenceInterface, index: number) => {
+                if (course.term === "FALL")
+                  return (
+                    <Tr key={"preference-" + index}>
+                      <Td>
+                        {course.subject} {course.code}
+                      </Td>
+                      <Td>{course.term}</Td>
+                      <Td>
+                        <RadioGroup
+                          id="canTeach"
+                          onChange={(v) => handleChange(course, "Able", v)}
+                        >
+                          <Stack direction="row">
+                            <Radio value="Able">Able</Radio>
+                            <Radio value="With Effort">With Effort</Radio>
+                          </Stack>
+                        </RadioGroup>
+                      </Td>
+                      <Td>
+                        <RadioGroup
+                          id="willingTeach"
+                          onChange={(v) =>
+                            handleChange(course, "Willingness", v)
+                          }
+                        >
+                          <Stack direction="row">
+                            <Radio value="Unwilling">Unwilling</Radio>
+                            <Radio value="Willing">Willing</Radio>
+                            <Radio value="Very Willing">Very Willing</Radio>
+                          </Stack>
+                        </RadioGroup>
+                      </Td>
+                    </Tr>
+                  );
+              }
             )}
           </Tbody>
         </Table>
