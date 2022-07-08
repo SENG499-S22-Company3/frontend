31affe2c2c84542c36567d864938c8ffcfd3a9ea
andrew polanyi <andrew.polanyi@gmail.com>
2022-07-08 10:41:50 -0700

    double filter now working

--- src/components/Schedule/SearchBar.tsx
+++ src/components/Schedule/SearchBar.tsx
@@ -20,27 +20,51 @@ interface SearchBarProps {
   getTermData: () => CourseSection[] | undefined;
   setScheduleData: (data: CourseSection[]) => void;
 }
-
+var courseFilter = "all";
+var termFilter = "all";
 export const SearchBar = (props: SearchBarProps) => {
   const { getTermData, setScheduleData } = props;
 
   let [searchInput, setSearchInput] = useState("");
   const [filtered, setFiltered] = useState("");
 
-  const toggleCourseTypes = (type: string) => {
-    if (type === "all") {
+  const toggleCourseTypes = (type: string, group: string) => {
+    if (group === "course") courseFilter = type;
+    else termFilter = type;
+
+    if (courseFilter == "all" && termFilter == "all") {
+      //both are all so give full list
       setSearchInput("");
       filterCourses(true);
       return;
-    } else {
+    } else if (type == "all") {
+      //all just selected, find out which group
+      if (group === "course") searchInput = termFilter;
+      else searchInput = courseFilter;
+      filterCourses();
+      return;
+    } else if (courseFilter == "all" || termFilter == "all") {
+      //only one filter
       searchInput = type;
       filterCourses();
+      return;
+    } else {
+      //double filtered
+      searchInput = courseFilter;
+      filterCourses(false, termFilter);
       return;
     }
   };
 
-  const filterCourses = (clear?: boolean) => {
-    const termData = getTermData();
+  const filterCourses = (
+    clear?: boolean,
+    next?: string,
+    td?: CourseSection[]
+  ) => {
+    let termData;
+    if (td) termData = td;
+    else termData = getTermData();
+
     if (!termData) return;
 
     if (searchInput === "" || clear) {
@@ -63,8 +87,15 @@ export const SearchBar = (props: SearchBarProps) => {
       );
       return inputWords.every((word) => appointmentValues.includes(word));
     });
-    setFiltered(searchInput);
-    setScheduleData(filteredAppointments);
+    if (!next || next == "") {
+      //single filter
+      setFiltered(searchInput);
+      setScheduleData(filteredAppointments);
+    } else {
+      //double filter present, run again
+      searchInput = next;
+      filterCourses(false, "", filteredAppointments);
+    }
   };
 
   return (
@@ -75,25 +106,25 @@ export const SearchBar = (props: SearchBarProps) => {
       <RadioGroup id="course_type" colorScheme="green" defaultValue={"all"}>
         <Stack direction="row">
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "course")}
             value="all"
           >
             All
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "course")}
             value="ece"
           >
             ECE
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "course")}
             value="seng"
           >
             SENG
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "course")}
             value="csc"
           >
             CSC
@@ -107,25 +138,25 @@ export const SearchBar = (props: SearchBarProps) => {
       <RadioGroup id="term" colorScheme="blue" defaultValue={"all"}>
         <Stack direction="row">
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "term")}
             value="all"
           >
             All
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "term")}
             value="summer"
           >
             Summer
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "term")}
             value="fall"
           >
             Fall
           </Radio>
           <Radio
-            onChange={(e) => toggleCourseTypes(e.target.value)}
+            onChange={(e) => toggleCourseTypes(e.target.value, "term")}
             value="spring"
           >
             Spring
