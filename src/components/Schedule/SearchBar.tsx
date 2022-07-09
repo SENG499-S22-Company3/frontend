48842b303930adbc79b7ce15e6f6509665db816a
andrew polanyi <andrew.polanyi@gmail.com>
2022-07-08 20:27:02 -0700

    reworked and simplyfied solution

--- src/components/Schedule/SearchBar.tsx
+++ src/components/Schedule/SearchBar.tsx
@@ -11,62 +11,38 @@ import {
   Radio,
   RadioGroup,
   Stack,
+  filter,
 } from "@chakra-ui/react";
 import { CourseSection } from "../../stores/schedule";
-import { Text } from "devextreme-react/linear-gauge";
-// import { RadioGroup } from "devextreme-react";
 
 interface SearchBarProps {
   getTermData: () => CourseSection[] | undefined;
   setScheduleData: (data: CourseSection[]) => void;
 }
 var courseFilter = "all";
+
 export const SearchBar = (props: SearchBarProps) => {
   const { getTermData, setScheduleData } = props;
 
-  let [searchInput, setSearchInput] = useState("");
-  // const [filtered, setFiltered] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  
+  const [filtered, setFiltered] = useState("");
 
-  const filterLogic = (type?: string, group?: string) => {
-    if (group === "course" && type) courseFilter = type;
-
-    if (courseFilter == "all" && searchInput == "") {
-      //give full list
-      setSearchInput("");
-      filterCourses(true);
-      return;
-    } else if (courseFilter == "all" && searchInput != "") {
-      //normal search with text from search input
-      filterCourses();
-      return;
-    } else if (courseFilter != "all" && searchInput == "") {
-      //only course filter
-      searchInput = courseFilter;
-      filterCourses();
-      return;
-    } else {
-      //double filtered with course and searchinput
-      filterCourses(false, courseFilter);
-      return;
-    }
-  };
-
-  const filterCourses = (
-    clear?: boolean,
-    next?: string,
-    td?: CourseSection[]
-  ) => {
-    let termData;
-    if (td) termData = td;
-    else termData = getTermData();
-
+  const filterCourses = (clear?: boolean) => {
+    const termData = getTermData();
     if (!termData) return;
 
-    if (searchInput === "" || clear) {
-      setScheduleData(termData);
-      return;
+    if (clear) {
+      if (courseFilter === "all"){
+        setFiltered("");
+        setScheduleData(termData);
+        return;
+      }else{
+        filter2(termData);
+        return;
+      }
     }
-
+    
     //checks if every word of the search exists as an attribute for the course
     const inputWords = searchInput.toLowerCase().split(" ");
     const filteredAppointments = termData.filter((course) => {
@@ -81,14 +57,36 @@ export const SearchBar = (props: SearchBarProps) => {
       );
       return inputWords.every((word) => appointmentValues.includes(word));
     });
-    if (!next || next == "") {
-      //single filter
-      setScheduleData(filteredAppointments);
-    } else {
-      //double filter present, run again
-      searchInput = next;
-      filterCourses(false, "", filteredAppointments);
-    }
+    setFiltered(searchInput);
+    if (courseFilter === "all")setScheduleData(filteredAppointments);
+    else filter2(filteredAppointments);
+  };
+  
+  const setFilter = (type:string)=>{
+      courseFilter = type;
+      filterCourses(false);
+  }
+
+  const filter2 = (td: CourseSection[]) => {
+    const termData = td;
+    if (!termData) return;
+
+    //checks if every word of the search exists as an attribute for the course
+    const inputWords = courseFilter;
+    const filteredAppointments = termData.filter((course) => {
+      const courseProperties = Object.values(course.CourseID);
+      const courseProfessors = course.professors.map(
+        (prof) => prof.displayName
+      );
+
+      let appointmentValues = "";
+      [...courseProperties, ...courseProfessors].forEach(
+        (value) => (appointmentValues += value.toLowerCase())
+      );
+      return appointmentValues.includes(inputWords);
+    });
+    setFiltered(searchInput);
+    setScheduleData(filteredAppointments);
   };
 
   return (
@@ -96,44 +94,43 @@ export const SearchBar = (props: SearchBarProps) => {
       <FormLabel htmlFor="course_type" mt={1}>
         Course:
       </FormLabel>
-      <RadioGroup id="course_type" colorScheme="green" defaultValue={"all"}>
+      <RadioGroup id="course_type" colorScheme="purple" defaultValue={"all"}>
         <Stack direction="row">
           <Radio
-            onChange={(e) => filterLogic(e.target.value, "course")}
+            onChange={(e) => setFilter(e.target.value)}
             value="all"
           >
             All
           </Radio>
           <Radio
-            onChange={(e) => filterLogic(e.target.value, "course")}
+            onChange={(e) => setFilter(e.target.value)}
             value="ece"
           >
             ECE
           </Radio>
           <Radio
-            onChange={(e) => filterLogic(e.target.value, "course")}
+            onChange={(e) => setFilter(e.target.value)}
             value="seng"
           >
             SENG
           </Radio>
           <Radio
-            onChange={(e) => filterLogic(e.target.value, "course")}
+            onChange={(e) => setFilter(e.target.value)}
             value="csc"
           >
             CSC
           </Radio>
         </Stack>
       </RadioGroup>
-
       <InputGroup marginX="2rem">
         <Input
           placeholder="Search"
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && filterLogic()}
+          onKeyDown={(e) => e.key === "Enter" && filterCourses()}
         />
         <InputRightElement>
-          {searchInput !== "" ? (
+          {searchInput !== "" && filtered === searchInput ? (
             <IconButton
               aria-label="Clear search"
               colorScheme={"red"}
@@ -147,7 +144,7 @@ export const SearchBar = (props: SearchBarProps) => {
             <IconButton
               aria-label="Search schedule"
               icon={<SearchIcon />}
-              onClick={() => filterLogic()}
+              onClick={() => filterCourses()}
             />
           )}
         </InputRightElement>
