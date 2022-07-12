7aa7cbaf26d2f5ed6daafc0e9c49a6e18bb19a48
andrew polanyi <andrew.polanyi@gmail.com>
2022-07-12 13:09:58 -0700

    merge conflict fix

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -197,36 +197,7 @@ export const Schedule = () => {
     >
       <Container mb={32} maxW="container.xl">
         <Heading mb={6}>View Schedule</Heading>
-        <Flex alignItems="center" justifyContent="space-between" mb={5}>
-          <Select
-            id="select"
-            w="16rem"
-            value={viewState}
-            onChange={(e) => {
-              refreshSchedule();
-              e.target.value === "table"
-                ? setViewState(ViewTypes.table)
-                : setViewState(ViewTypes.calendar);
-            }}
-          >
-            <option value="table">Table View</option>
-            <option value="calendar">Calendar View</option>
-          </Select>
-          <SearchBar
-            getTermData={getScheduleRef}
-            setScheduleData={setScheduleData}
-          />
-          <Button
-            w="300px"
-            as={Link}
-            to="/schedule"
-            backgroundColor="purple.300"
-            colorScheme="purple"
-            variant="solid"
-          >
-            Generate New Schedule
-          </Button>
-        </Flex>
+        
         {!scheduleData || scheduleLoading ? (
           <Container
             display="flex"
