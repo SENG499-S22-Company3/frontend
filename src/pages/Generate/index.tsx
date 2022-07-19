1fa743a2d7d603ecb064e8c79eaf1526e29f054f
Jayden Chan <jaydencn7@gmail.com>
2022-07-18 22:26:41 -0700

    feat: Update generate page wip

--- src/pages/Generate/index.tsx
+++ src/pages/Generate/index.tsx
@@ -21,6 +21,9 @@ import {
   useColorModeValue,
   useToast,
 } from "@chakra-ui/react";
+
+import courseTerms from "./courseTerms.json";
+
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import {
@@ -73,9 +76,11 @@ interface CourseInput {
   section: number;
 }
 
+type Term = "FALL" | "SPRING" | "SUMMER" | "WHOLE_YEAR";
+
 export const Generate = () => {
   const [year, setYear] = useState("");
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState<Term | undefined>(undefined);
   const [courseOptions, setCourseOptions] = useState<Array<CourseOption>>([]);
   const [selectedCourses, setSelectedCourses] = useState<Array<CourseInput>>(
     []
@@ -83,47 +88,88 @@ export const Generate = () => {
   const [algorithm1, setAlgorithm1] = useState("Company 3");
   const [algorithm2, setAlgorithm2] = useState("Company 3");
   const [generate, { data, loading, error }] = useMutation(GENERATE);
+
   const {
     data: courseData,
     loading: courseLoading,
     error: courseError,
   } = useQuery(GET_COURSES);
+
   const navigate = useNavigate();
 
   const bg = useColorModeValue("gray.50", "gray.700");
-
   const bgDark = useColorModeValue("gray.50", "gray.800");
 
   const toast = useToast();
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const courses = {
+      FALL: term === "FALL" ? selectedCourses : [],
+      SPRING: term === "SPRING" ? selectedCourses : [],
+      SUMMER: term === "SUMMER" ? selectedCourses : [],
+    };
+
+    if (term === "WHOLE_YEAR") {
+      for (const course of selectedCourses) {
+        const codeString = `${course.subject}${course.code}`;
+
+        // the React type checker has an aneurism when trying to deal with this imported
+        // JSON for some reason.
+        const normallyOfferedTerms = courseTerms[
+          codeString as keyof typeof courseTerms
+        ] as unknown as ("FALL" | "SPRING" | "SUMMER")[] | undefined;
+
+        if (!normallyOfferedTerms) {
+          toast({
+            status: "error",
+            description: `Failed to determine normally offered term of ${codeString}`,
+            isClosable: true,
+          });
+          return;
+        }
+
+        if (course.section === 0 || normallyOfferedTerms.length === 1) {
+          normallyOfferedTerms.forEach((term) => {
+            courses[term].push(course);
+          });
+        } else {
+          const sectionAmounts = Array(normallyOfferedTerms.length).fill(0);
+          for (let i = 0; i < course.section; i++) {
+            sectionAmounts[i % sectionAmounts.length] += 1;
+          }
+
+          normallyOfferedTerms.forEach((term, i) => {
+            courses[term].push({ ...course, section: sectionAmounts[i] });
+          });
+        }
+      }
+    }
+
     const input = {
       year: Number(year),
-      term: term,
-      courses: selectedCourses,
+      fallCourses: courses.FALL,
+      springCourses: courses.SPRING,
+      summerCourses: courses.SUMMER,
       algorithm1: algorithm1,
       algorithm2: algorithm2,
     };
-    console.log(input);
+    console.log(JSON.stringify(input, null, 2));
     generate({ variables: { input } });
   };
 
   const handleCourseChange = (
-    courses: MultiValue<CourseOption>,
+    _courses: MultiValue<CourseOption>,
     actionMeta: ActionMeta<CourseOption>
   ) => {
     if (actionMeta.action === "select-option") {
-      if (actionMeta.option) {
-        const newSelected = [
-          ...selectedCourses,
-          {
-            code: actionMeta.option.value.code,
-            subject: actionMeta.option.value.subject,
-            section: 0,
-          },
-        ];
-        setSelectedCourses(newSelected);
+      if (actionMeta.option !== undefined) {
+        const newCourse = {
+          code: actionMeta.option.value.code,
+          subject: actionMeta.option.value.subject,
+          section: 0,
+        };
+        setSelectedCourses((prev) => [...prev, newCourse]);
       } else {
         toast({
           title: "Option not defined",
@@ -133,14 +179,12 @@ export const Generate = () => {
       }
     } else if (actionMeta.action === "remove-value") {
       if (actionMeta.removedValue) {
-        setSelectedCourses(
-          selectedCourses.filter((course) => {
-            return (
-              course.code !== actionMeta.removedValue.value.code &&
-              course.subject !== actionMeta.removedValue.value.subject
-            );
-          })
-        );
+        const val = actionMeta.removedValue.value;
+        setSelectedCourses((prev) => {
+          return prev.filter(
+            (c) => c.code !== val.code || c.subject !== val.subject
+          );
+        });
       } else {
         toast({
           title: "Failed to remove course",
@@ -178,11 +222,17 @@ export const Generate = () => {
   useEffect(() => {
     if (!courseLoading) {
       if (!courseError && courseData) {
-        const options = courseData.survey.courses
-          .filter((course: CourseSection) => {
-            return course.term === term;
+        const courseKeys = new Set<string>();
+        const options = (courseData.survey.courses as CourseSection[])
+          .filter((course) => {
+            const key = course.subject + course.code;
+            if (courseKeys.has(key)) {
+              return false;
+            }
+            courseKeys.add(key);
+            return term === "WHOLE_YEAR" || course.term === term;
           })
-          .map((course: CourseSection) => {
+          .map((course) => {
             return {
               label: course.subject + " " + course.code,
               value: {
@@ -191,7 +241,15 @@ export const Generate = () => {
               },
             };
           });
+
         setCourseOptions(options);
+        setSelectedCourses(
+          options.map((o) => ({
+            code: o.value.code,
+            subject: o.value.subject,
+            section: 0,
+          }))
+        );
       } else {
         console.log(courseError);
       }
@@ -212,7 +270,7 @@ export const Generate = () => {
         } else {
           console.log(data);
           toast({
-            title: "Failed to Generate Schedule",
+            title: "Failed to generate Schedule",
             description: data.generateSchedule.message,
             status: "error",
             isClosable: true,
@@ -221,7 +279,7 @@ export const Generate = () => {
       } else if (error) {
         console.log(error);
         toast({
-          title: "Failed to Generate Schedule",
+          title: "Failed to generate Schedule",
           description: error.message,
           status: "error",
           isClosable: true,
@@ -238,9 +296,9 @@ export const Generate = () => {
       justifyContent="center"
       flexDirection="column"
     >
-      <Heading mb={10}>Generate Schedule</Heading>
+      <Heading my={10}>Generate Schedule</Heading>
       <Container
-        minW="900px"
+        minW="container.xl"
         bg={bg}
         borderRadius={20}
         p={10}
@@ -253,7 +311,7 @@ export const Generate = () => {
             <Grid templateColumns="repeat(2, 1fr)" gap={10}>
               <GridItem
                 bg={bgDark}
-                minW="450px"
+                minW="container.md"
                 p={10}
                 borderRadius={10}
                 flexDir="column"
@@ -278,10 +336,11 @@ export const Generate = () => {
                 <FormLabel htmlFor="term">Term</FormLabel>
                 <Select
                   placeholder="Select Term"
-                  onChange={(e) => setTerm(e.target.value)}
+                  onChange={(e) => setTerm(e.target.value as Term)}
                   mb={5}
                   w="50%"
                 >
+                  <option value="WHOLE_YEAR">Whole Year</option>
                   <option value="SPRING">Spring</option>
                   <option value="SUMMER">Summer</option>
                   <option value="FALL">Fall</option>
@@ -290,6 +349,12 @@ export const Generate = () => {
                 <SelectPlus<CourseOption, true, GroupBase<CourseOption>>
                   isMulti
                   name="courses"
+                  value={courseOptions.filter((c) =>
+                    selectedCourses.some(
+                      (s) =>
+                        s.code === c.value.code && s.subject === c.value.subject
+                    )
+                  )}
                   options={courseOptions}
                   placeholder="Select courses"
                   onChange={handleCourseChange}
@@ -322,7 +387,25 @@ export const Generate = () => {
                     <Radio value="COMPANY4">Company 4</Radio>
                   </Stack>
                 </RadioGroup>
+                <Flex
+                  flexDirection="column"
+                  w="100%"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Button
+                    mt={5}
+                    w="300px"
+                    colorScheme="blue"
+                    variant="solid"
+                    type="submit"
+                    isLoading={loading}
+                  >
+                    Generate Schedule
+                  </Button>
+                </Flex>
               </GridItem>
+
               <GridItem
                 bg={bgDark}
                 p={10}
@@ -339,7 +422,7 @@ export const Generate = () => {
                   <Thead>
                     <Tr>
                       <Th>Course</Th>
-                      <Th># Sections</Th>
+                      <Th># of Sections</Th>
                     </Tr>
                   </Thead>
                   <Tbody>
@@ -352,10 +435,9 @@ export const Generate = () => {
                                 onChange={(e) =>
                                   handleSectionChange(course, e.target.value)
                                 }
+                                defaultValue="0"
                               >
-                                <option selected value="0">
-                                  Default
-                                </option>
+                                <option value="0">Auto</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -375,16 +457,6 @@ export const Generate = () => {
                 </Table>
               </GridItem>
             </Grid>
-            <Button
-              mt={5}
-              w="300px"
-              colorScheme="blue"
-              variant="solid"
-              type="submit"
-              isLoading={loading}
-            >
-              Generate Schedule
-            </Button>
           </FormControl>
         </form>
       </Container>
