c9b46d65e0f18e47721b9d58bce4934e31caa574
nicole <nicolelynneudy@gmail.com>
2022-07-18 21:14:21 -0700

    Revert "created design for 404 page"
    This reverts commit 13c8afbd6ac152a3b519027f7c2afe6620aa1ba0.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,5 @@
-import { Flex, Container, Heading, Button, Text } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import notFound from "../404.jpg";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
 
 export const NotFound = () => {
   return (
@@ -12,33 +11,7 @@ export const NotFound = () => {
       justifyContent="center"
       flexDirection="column"
     >
-      <Container
-        w="30%"
-        borderRadius={20}
-        p={10}
-        style={{ boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.40)" }}
-        textAlign="center"
-        centerContent
-      >
-        <Heading size="4xl" mb={2}>
-          404
-        </Heading>
-        <Text fontWeight="500" pb="10px" mb={5}>
-          Page not found
-        </Text>
-        <img src={notFound} alt="logo" />
-        <Link to="/">
-          <Button
-            mt={10}
-            type="submit"
-            colorScheme="blue"
-            variant="solid"
-            w="100%"
-          >
-            Go Home
-          </Button>
-        </Link>
-      </Container>
+      404
     </Flex>
   );
 };
